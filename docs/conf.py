0d79ac5ba203167dc9eb08496b662ea532337c63
sreyemnayr <ryanmeyersweb@gmail.com>
2018-02-02 14:20:13 -0600

    Will this work?

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../jssapi/'))
 
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
