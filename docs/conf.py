0c27016efd6c00eaecf6ed796b0442d51409c99e
sreyemnayr <ryanmeyersweb@gmail.com>
2018-02-02 13:56:36 -0600

    Import modules

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import os
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
@@ -32,6 +32,8 @@ project_root = os.path.dirname(cwd)
 sys.path.insert(0, project_root)
 
 import jssapi
+import jssapi.jssapi
+import jssapi.cph
 
 # -- General configuration ---------------------------------------------
 
