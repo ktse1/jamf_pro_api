f4e9cdb244619e4e599f33d973fb279804b8e071
sreyemnayr <ryanmeyersweb@gmail.com>
2018-02-02 09:16:18 -0600

    First commit

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use jamf Pro API in a project::
+
+    import jamf_pro_api
---
+++ jamf_pro_api/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for jamf Pro API."""
+
+__author__ = """Ryan Meyers"""
+__email__ = 'ryanmeyersweb@gmail.com'
+__version__ = '0.1.0'
